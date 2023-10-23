290621c125f765d8e31ef28cba8218f027f0971c
YongLo <45794006+YongLo@users.noreply.github.com>
2023-10-23 17:23:30 -0400

    Add read file to dictionary
    read file and convert to dictionary

---
+++ InputManager.py
@@ -0,0 +1,60 @@
+from TwitchPlays_KeyCodes import *
+import inspect, re
+
+''' file format
+up  HR:W
+down  HR:S
+left  HR:A
+right  HR:D
+shoot up  R:J-H-K  H:U
+shoot down  R:U-H-K  H:J
+shoot left  R:U-J-K  H:H
+shoot right  R:U-J-H  H:K
+be a man  R:U-J-H-K
+'''
+
+
+#write/save input into dictionary (in progress..)
+def writeFile(inputDic, inputCommand):
+  return 
+
+#read/load input from file and transfer to dictionary
+#@inputFile - name of file
+#return - dictionary
+def readFile(inputFile):
+  inputList = {}
+  with open(inputFile, "r") as file:
+    for line in file:
+      line = line.strip().split("  ", 1)      
+      inputName, inputCommands = line[0], line[1]
+      inputList[inputName] = readCommands(inputCommands, ":")
+  return inputList
+
+#read commands string and transfer to dictionary
+#Note: each command keys are stored as a variable name (output will show binary convertion of the hex value of key that was assign to it)
+#@inputCommands - string of commands
+#@splitBy - probably don't need
+#return dictionary of type of commands with array list of keys
+def readCommands(inputCommands, splitBy):
+  commandList = {}
+  for line in inputCommands.split():
+    commandType, keys = line.split(splitBy)
+    keyList = keys.split("-")
+    for i, char in enumerate(keyList):
+      keyList[i] = globals()[char]
+    commandList[commandType] = keyList
+  return commandList
+
+#print formated version of dictionary
+def print_command(dct):
+  print("Command List:")
+  for inputName, inputCommand in dct.items():  # dct.iteritems() in Python 2
+    print("{} {}".format(inputName, inputCommand))
+
+#Print variable name
+#https://stackoverflow.com/questions/592746/how-can-you-print-a-variable-name-in-python
+def varname(p):
+  for line in inspect.getframeinfo(inspect.currentframe().f_back)[3]:
+    m = re.search(r'\bvarname\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
+    if m:
+      return m.group(1)
