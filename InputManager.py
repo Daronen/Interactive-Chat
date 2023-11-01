77007812495851ce32bdcc09049794d45968a14d
YongLo <45794006+YongLo@users.noreply.github.com>
2023-10-31 22:02:31 -0400

    Commenting and change functions name
    Add comment trying to explain each function uses, and what each parameter means
    Changed writeCommand to writeFile because it writing onto a file not a command.

--- InputManager.py
+++ InputManager.py
@@ -25,29 +25,33 @@ def readFile(inputFile):
       #split line of string from, command name : command dectionary
       line = line.strip().split("  ", 1)      
       inputName, inputCommands = line[0], line[1]
-      inputList[inputName] = readCommands(inputCommands, ":")
+      inputList[inputName] = readCommands(inputCommands)
   return inputList
 
 
 #read commands string and transfer to dictionary
-#Note: each command keys are stored as a variable name (output will show binary convertion of the hex value of key that was assign to it)
 #@inputCommands - string of commands
-#@splitBy - probably don't need, but split string by certain character
 #return dictionary of type of commands with array list of keys
-def readCommands(inputCommands, splitBy):
+def readCommands(inputCommands):
   commandList = {}
+  #split each type of commands on a line
   for line in inputCommands.split():
-    commandType, keys = line.split(splitBy)
+    #split each command type from keys into a dictionary
+    commandType, keys = line.split(":")
+
+    #split each key into a list of keys
     keyList = keys.split("-")
     for i, char in enumerate(keyList):
       keyList[i] = char
-      #print(varname(globals()[]))
     commandList[commandType] = keyList
   return commandList
   
-#
-def writeCommand(fileName, inputDic):
+#write command dictionary into file
+#@fileName - name of file
+#@inputDic - dictionary of commands
+def writeFile(fileName, inputDic):
   file = open(fileName, "a")
+  #get through each part of nested dictionary/ array into a string and write to file
   for commandKey in inputDic:
     curStr = commandKey;
     for commandType in inputDic[commandKey]:
@@ -59,23 +63,49 @@ def writeCommand(fileName, inputDic):
     file.write(curStr + "\n")
   file.close      
 
+#empty out file
+#@fileName - name of file wanted to be emptied
+def emptiedFile(fileName):
+  #seen pointless to make function for it
+  open('writeInput.txt', 'w').close()
 
 #add command to dictionary
+#@inputDic - Distionary of commands
+#@inputName - the name of the command/ string that chat have to input to call the command
+#@inputType -  Type of input function to call, like...
+#             (HR(HoldAndReleaseKey), 
+#              H (HoldKey), 
+#              R (Release)) 
+#@inputKey - keyboards keys that wanted to use, like...
+#             W,A,S,D
 def addCommand(inputDic, inputName, inputType, inputKey):
+  #check if inputType exist in dictionary
   if searchCommand(inputDic, inputName, inputType):
     inputDic[inputName][inputType].append(inputKey) 
   else:
+    #if not, lets check where to start adding in the dictionary
+    #if inputName doesn't exist in dictionary, lets add it with everything else
     if inputName not in inputDic:
       inputDic[inputName] = {inputType: [inputKey]}
     else:
+      #if inputType doesn't exist in dictionary, add it and add keys
       if inputType not in inputDic[inputName]:
         inputDic[inputName][inputType] = [inputKey]
       else:
+        #add new empty array and the 
+        #probably don't need this
         inputDic[inputName][inputType] = []
         inputDic[inputName][inputType].append(globals()[inputKey]) 
   
 
 #search if command exist in the dictionary
+##@inputDic - Distionary of commands
+#@inputName - the name of the command/ string that chat have to input to call the command
+#@inputType -  Type of input function to call, like...
+#             (HR(HoldAndReleaseKey), 
+#              H (HoldKey), 
+#              R (Release)) 
+#return - True if command exist, False if not
 def searchCommand(inputDic, inputName, inputType):
   isExist = False
   if inputName in inputDic:
@@ -84,14 +114,22 @@ def searchCommand(inputDic, inputName, inputType):
       isExist = True
   return isExist
 
-#remove command from dictionary (in progress ...)
+#remove command from dictionary (SHOULD CHANGE IF THING GOES MESSY!)
 #Note: if inputKey doesn't exist in array list, it remove the inputType dictionary
 #      if inputType doesn't exist in dictionary, it remove the inputName dictionary
+#@inputDic - Distionary of commands
+#@inputName - the name of the command/ string that chat have to input to call the command
+#@inputType -  Type of input function to call, like...
+#             (HR(HoldAndReleaseKey), 
+#              H (HoldKey), 
+#              R (Release)) 
+#@inputKey - keyboards keys that wanted to use, like...
+#             W,A,S,D
 def removeCommand(inputDic, inputName, inputType, inputKey):
   if inputType in inputDic[inputName]:
-    if globals()[inputKey] in inputDic[inputName][inputType]:  
-      
-      #remove first element similar to inputType
+    if inputKey in inputDic[inputName][inputType]:  
+      #remove first key element similar from array
+      print(inputKey);
       inputDic[inputName][inputType].remove(inputKey)
     else:
       del inputDic[inputName][inputType]
